3c326ccdeefe79a01d9bd168669fc42ed8cd7185
Raphael Borba <raphaelbsx@gmail.com>
2022-12-09 19:24:31 -0300

    feat: getGames return with category_name;

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,19 +1,21 @@
 import { connection } from "../database/db.js";
 
-export async function getGames(req, res){
+export async function getGames(req, res) {
 
-    const {name} = req.query
+    const { name } = req.query
 
     try {
-        
-        if(name){
+
+        if (name) {
 
             const games = await connection.query("SELECT * FROM games WHERE name LIKE ($1 || '%');", [name])
 
             return res.send(games.rows).status(200);
         }
 
-        const games = await connection.query("SELECT * FROM games;")
+        const games = await connection.query(`SELECT games.*, categories.name AS category_name 
+                                                FROM games 
+                                                JOIN categories ON category_id=categories.id`)
 
         res.send(games.rows).status(200)
 
@@ -23,14 +25,14 @@ export async function getGames(req, res){
     }
 }
 
-export async function postGames(req, res){
+export async function postGames(req, res) {
 
-    const {name, image, stockTotal, categoryId, pricePerDay} = req.body;
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
 
     try {
-        
+
         await connection.query('INSERT INTO games(name, image, stock_total, category_id, price_per_day) VALUES ($1,$2,$3,$4,$5);',
-        [name, image, stockTotal, categoryId, pricePerDay])
+            [name, image, stockTotal, categoryId, pricePerDay])
 
         res.sendStatus(200)
 
