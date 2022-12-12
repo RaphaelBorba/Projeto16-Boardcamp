9da72bab3744e2fc1dbf64e938dd5849a84ada29
Raphael Borba <raphaelbsx@gmail.com>
2022-12-12 00:22:03 -0300

    fix: add response

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -114,6 +114,8 @@ export async function deleteRental(req, res){
     try {
         
         await connection.query(`DELETE FROM rentals WHERE id=$1;`, [id])
+
+        res.sendStatus(200)
         
     } catch (error) {
         console.log(error)
